1df26c8dc9a93663bd752e35361944a438d425ed
Dhruv Rai <dhruv.guitarist@gmail.com>
2012-12-10 13:26:07 +0000

    My First Working Page
    Index page working.. But have to input Google Books API..

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php require_once("common/header.php");?>
+			<center><img src="images/header.jpg" width="800" height="250" style="margin-top:-20px;"/></center>
+			<section id="body" class="container_24">
+				<div class="grid_24">
+					
+				</div>
+				<div class="clear"></div>
+				<div class="grid_1">&nbsp;</div>
+				<div class="grid_9">
+					<hgroup><h2>Best online book store in world</h2></hgroup>
+					<p>This is the best ebook site ever.</p>
+				</div>
+				<div class="grid_14">
+					<img src="images/home.jpg" width="550" height="315"/>
+				</div>
+				<div class="clear"></div>
+				<div class="grid_24">
+					&nbsp;
+				</div>
+				<div class="clear"></div>
+				<div class="grid_1">&nbsp;</div>
+                                <form action="registration.php" method="post">
+				<div class="grid_22 stock">
+					Click here to register to our online library  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<button class="join red" style="background-color:red;">Register</button>
+				</div>
+                               </form>
+				<div class="grid_1">&nbsp;</div>
+				<div class="clear"></div>
+				<div class="grid_24">
+					<center><hgroup><h1>Input Books API</h1></hgroup></center>
+				</div>
+				<div class="grid_24">
+<?php require_once("common/footer.php");?>
