d731ae866d29e64b6df1a9230f39ff8babe94d68
Dhruv Rai <dhruv.guitarist@gmail.com>
2012-12-17 00:44:41 +0000

    api changes

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,11 @@
 <?php require_once("common/header.php");?>
+<?php
+if(isset($_POST['search'])){
+	$search_text = $_POST['search'];
+}else{
+	$search_text = "Deployment";
+}
+?>
 	<section id="body" class="container_24">
 		<div class="grid_18">&nbsp;</div>
 		<div class="grid_5" style="text-align:right;">Hi <?php echo $_SESSION['name'];?>!</div>
@@ -8,6 +15,10 @@
 		<div class="grid_1">&nbsp;</div>
 		<div class="grid_22">
 			<p>You are the staff user. You all have the rights to add books.<br>To add books please select the "Add Books" link on the top.<br>Also  you can read all the books fro the link above "Read Books".</p>
+			<form name="searchForm" method="post" action="">
+			<input type="text" value="<?php echo $search_text;?>" name="search">
+			<button type="submit">Submit</button>
+			</form>
 		<div id="content"></div>
 			<script>
       function handleResponse(response) {
@@ -24,11 +35,7 @@
 		author[i] =  item.volumeInfo.authors[0];
 		rating[i] =  item.volumeInfo.averageRating;
         // in production code, item.text should have the HTML entities escaped.
-         document.getElementById("content").innerHTML += "<tr>";
-		 document.getElementById("content").innerHTML += "<td><a href = '" + item.accessInfo.webReaderLink + "' target='_blank'>" + item.volumeInfo.title + "</a></td>";
-		 document.getElementById("content").innerHTML += "<td>" + item.volumeInfo.authors[0] + "</td>";
-		 document.getElementById("content").innerHTML += "<td>Rating: " + item.volumeInfo.averageRating + "</td>";
-		 document.getElementById("content").innerHTML += "</tr>";
+         document.getElementById("content").innerHTML += "<a href = '" + item.accessInfo.webReaderLink + "' target='_blank'>" + item.volumeInfo.title + "</a> by " + item.volumeInfo.authors[0] + "<br>"
       }
 	  document.getElementById("content").innerHTML += "</table>";
 	  
@@ -52,8 +59,15 @@
 		xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 		xmlhttp.send("link="+link+"&name="+name+"&author="+author+"&rating="+rating);
     }
+	var search_text = "<?php echo urlencode($search_text);?>";
     </script>
-    <script src="https://www.googleapis.com/books/v1/volumes?q=india&callback=handleResponse"></script>
+	<?php
+	if ($search_text == "Deployment"){
+	?>
+    <script src='https://www.googleapis.com/books/v1/volumes?q=Deployment&callback=handleResponse'></script>
+	<?php }else if ($search_text == "cloud computing"){?>
+    <script src='https://www.googleapis.com/books/v1/volumes?q=cloud+computing&callback=handleResponse'></script>
+	<?php }?>
 		</div>
 		<div class="grid_1">&nbsp;</div>
 		<div class="clear"></div>
