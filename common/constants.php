9486bffa0958209c133d1f958dd7ee8d7ad75bdf
Dhruv Rai <dhruv.guitarist@gmail.com>
2012-12-17 14:02:02 +0000

    DB

--- common/constants.php
+++ common/constants.php
@@ -1,6 +1,16 @@
+// <?php
+//	define("DB_SERVER","int.instance34525.db.xeround.com.:3107");
+//	define("DB_USER","50cf192bdc6e4");
+//	define("DB_Pass","8da6f5a847973b7f797ad029613a183fc5abc5c9");
+//	define("DB_NAME","orchestra");
+// ?>
+
+
 <?php
-	define("DB_SERVER","int.instance34525.db.xeround.com.:3107");
-	define("DB_USER","50cf192bdc6e4");
-	define("DB_Pass","8da6f5a847973b7f797ad029613a183fc5abc5c9");
-	define("DB_NAME","orchestra");
+  $con = mysql_pconnect("int.instance34525.db.xeround.com.:3107");
+  if (!$con)
+    die('Could not connect: ' . mysql_error());
+  mysql_select_db("mydb");
+
+ 
 ?>
