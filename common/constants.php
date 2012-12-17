bf02a46d6ceb0da55b6d8f12d5107bbf31fc20b8
Dhruv Rai <dhruv.guitarist@gmail.com>
2012-12-17 13:07:36 +0000

    Orchestra

--- common/constants.php
+++ common/constants.php
@@ -1,5 +1,5 @@
 <?php
-	define("DB_SERVER","instance34482.db.xeround.com:3059");
+	define("DB_SERVER","instance34482.db.xeround.com");
 	define("DB_USER","50ce6ee4346e1");
 	define("DB_Pass","febb5c615624d0762a573beb7e3d94eaa1f62788");
 	define("DB_NAME","orchestra");
