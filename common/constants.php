6402a0cc5c70e4cf69ec90ace27109099db7ca75
Dhruv Rai <dhruv.guitarist@gmail.com>
2012-12-17 13:15:44 +0000

    db change

--- common/constants.php
+++ common/constants.php
@@ -1,6 +1,6 @@
 <?php
-	define("DB_SERVER","instance34482.db.xeround.com");
-	define("DB_USER","50ce6ee4346e1");
-	define("DB_Pass","febb5c615624d0762a573beb7e3d94eaa1f62788");
+	define("DB_SERVER","instance34523.db.xeround.com:3106");
+	define("DB_USER","50cf192bdc6e4");
+	define("DB_Pass","8da6f5a847973b7f797ad029613a183fc5abc5c9");
 	define("DB_NAME","orchestra");
 ?>
